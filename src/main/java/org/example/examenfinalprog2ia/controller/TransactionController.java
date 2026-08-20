156106a919227ac984b212ec82b7014dda4538e7
Mrbrokoly <hei.tommy.3@gmail.com>
2026-08-20 17:06:32 +0300

    add TransactionController

--- src/main/java/org/example/examenfinalprog2ia/controller/TransactionController.java
+++ src/main/java/org/example/examenfinalprog2ia/controller/TransactionController.java
@@ -1,8 +1,9 @@
 package org.example.examenfinalprog2ia.controller;
 
-
 import lombok.RequiredArgsConstructor;
 import org.example.examenfinalprog2ia.model.Transaction;
+import org.example.examenfinalprog2ia.model.TransactionType;
+import org.example.examenfinalprog2ia.service.TransactionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,10 +24,8 @@ public class TransactionController {
         return ResponseEntity.ok(transactions);
     }
 
-
     @PostMapping("/transaction")
-    public ResponseEntity<List<Transaction>> creatpublic class TransactionController {
-}eTransactions(
+    public ResponseEntity<List<Transaction>> createTransactions(
             @RequestBody List<Transaction> transactions) {
 
         List<Transaction> created = transactionService.saveAll(transactions);
