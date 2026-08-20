5a154df000bc807344cb627b5bcf5568decd93e9
Mrbrokoly <hei.tommy.3@gmail.com>
2026-08-20 15:19:37 +0300

    first commit

---
+++ src/main/java/org/example/examenfinalprog2ia/ExamenFinalProg2IaApplication.java
@@ -0,0 +1,13 @@
+package org.example.examenfinalprog2ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalProg2IaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFinalProg2IaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/examenfinalprog2ia/ExamenFinalProg2IaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.examenfinalprog2ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalProg2IaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
