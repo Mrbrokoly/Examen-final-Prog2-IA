fc5e0ad33858bd36bcb7f5b8df4cac7bfc7418ba
AzariahMartinault <hei.azariah@gmail.com>
2026-08-20 15:50:42 +0200

    fix: add .bulider in Account

--- src/main/java/org/example/examenfinalprog2ia/model/Account.java
+++ src/main/java/org/example/examenfinalprog2ia/model/Account.java
@@ -1,9 +1,11 @@
 package org.example.examenfinalprog2ia.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class Account {
     private String id;
